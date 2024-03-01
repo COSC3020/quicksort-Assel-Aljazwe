7762329f142cd0acf47275f65e518a394519fa07
Assel-Aljazwe <157559559+Assel-Aljazwe@users.noreply.github.com>
2024-02-29 23:37:23 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,31 @@
 function quicksort(array) {
+    // Stack to simulate the recursive calls
+    let stack = [[0, array.length - 1]];
+
+    while (stack.length) {
+        const [start, end] = stack.pop();
+        if (start >= end) continue;
+
+        const pivotIndex = partition(array, start, end);
+
+        // Push subarrays defined by pivotIndex back onto the stack
+        stack.push([start, pivotIndex - 1]);
+        stack.push([pivotIndex + 1, end]);
+    }
+
     return array;
+}
+
+function partition(array, start, end) {
+    const pivotValue = array[end];
+    let pivotIndex = start;
+    for (let i = start; i < end; i++) {
+        if (array[i] < pivotValue) {
+            [array[i], array[pivotIndex]] = [array[pivotIndex], array[i]];
+            pivotIndex++;
+        }
+    }
+    // Swap pivot to the correct position
+    [array[pivotIndex], array[end]] = [array[end], array[pivotIndex]];
+    return pivotIndex;
 }
